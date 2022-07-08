82b54a4f6aa368bb70e78e9302a669b136d91095
gaurav kumar <gauravkumar@gauravs-MacBook-Air.local>
2022-07-08 20:26:19 +0530

    remote execution of jar using mvn

--- src/main/java/com/thoughtworks/gauge/example/java/Main.java
+++ src/main/java/com/thoughtworks/gauge/example/java/Main.java
@@ -2,14 +2,10 @@ package com.thoughtworks.gauge.example.java;
 
 import org.apache.maven.shared.invoker.*;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Collections;
-import java.util.Enumeration;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
 public class Main {
 
@@ -35,11 +31,11 @@ public class Main {
         request.setGoals( Collections.singletonList( "test" ) );
 
         Invoker invoker = new DefaultInvoker();
-        invoker.setMavenHome(new File(System.getenv("MAVEN_HOME")));
+        invoker.setMavenHome(new File("/usr/local/Cellar/maven/3.8.6/libexec"));
         invoker.execute( request );
     }
 
-    public static void main(String[] args) throws MavenInvocationException {
+    public static void main(String[] args) throws IOException, MavenInvocationException {
 //        ClassLoader classLoader = Main.class.getClassLoader();
 //        String cmd = "gauge run "+ classLoader.getResource("specs").getPath();
 
